1b0612213122daa3981453f48a32eda717db4b70
amitasingh <amitasingh3173@gmail.com>
2024-11-09 23:41:21 +0530

    bugs resolved

--- src/app/report/[type]/page.tsx
+++ src/app/report/[type]/page.tsx
@@ -5,81 +5,28 @@ import './ReportPage.css'
 import { AiFillEdit } from 'react-icons/ai'
 import CaloriIntakePopup from '@/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup';
 
-const page = () => {
-    const color = '#ffc20e'
-
+const Page = () => {
+    const color = '#ffc20e';
     const chartsParams = {
-        // margin: { bottom: 20, left: 25, right: 5 },
         height: 300,
-
     };
 
-    const [dataS1, setDataS1] = React.useState<any>(null)
+    const [dataS1, setDataS1] = React.useState<any>(null);
+
     const getDataForS1 = async () => {
+        const temp = [
+            { date: 'Thu Sep 28 2023 20:30:30 GMT+0530', value: 2000, unit: 'kcal' },
+            { date: 'Wed Sep 27 2023 20:30:30 GMT+0530', value: 2500, unit: 'kcal' },
+            { date: 'Tue Sep 26 2023 20:30:30 GMT+0530', value: 2700, unit: 'kcal' },
+            { date: 'Mon Sep 25 2023 20:30:30 GMT+0530', value: 3000, unit: 'kcal' },
+            { date: 'Sun Sep 24 2023 20:30:30 GMT+0530', value: 2000, unit: 'kcal' },
+            { date: 'Sat Sep 23 2023 20:30:30 GMT+0530', value: 2300, unit: 'kcal' },
+            { date: 'Fri Sep 22 2023 20:30:30 GMT+0530', value: 2500, unit: 'kcal' },
+            { date: 'Thu Sep 21 2023 20:30:30 GMT+0530', value: 2700, unit: 'kcal' },
+        ];
 
-        let temp = [
-            {
-                date: 'Thu Sep 28 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2000,
-                unit: 'kcal'
-            },
-            {
-                date: 'Wed Sep 27 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2500,
-                unit: 'kcal'
-            },
-            {
-                date: 'Tue Sep 26 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2700,
-                unit: 'kcal'
-            },
-            {
-                date: 'Mon Sep 25 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 3000,
-                unit: 'kcal'
-            },
-            {
-                date: 'Sun Sep 24 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2000,
-                unit: 'kcal'
-            },
-            {
-                date: 'Sat Sep 23 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2300,
-                unit: 'kcal'
-            },
-            {
-                date: 'Fri Sep 22 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2500,
-                unit: 'kcal'
-            },
-            {
-                date: 'Thu Sep 21 2023 20:30:30 GMT+0530 (India Standard Time)',
-                value: 2700,
-                unit: 'kcal'
-            },
-        ]
-
-        let dataForLineChart = temp.map((item: any) => {
-            let val = JSON.stringify(item.value)
-            return val
-        })
-
-        let dataForXAxis = temp.map((item: any) => {
-            let val = new Date(item.date)
-            return val
-        })
-
-        console.log({
-            data: dataForLineChart,
-            title: '1 Day Calorie Intake',
-            color: color,
-            xAxis: {
-                data: dataForXAxis,
-                label: 'Last 10 Days',
-                scaleType: 'time'
-            }
-        })
+        const dataForLineChart = temp.map(item => item.value);
+        const dataForXAxis = temp.map(item => new Date(item.date));
 
         setDataS1({
             data: dataForLineChart,
@@ -90,131 +37,53 @@ const page = () => {
                 label: 'Last 10 Days',
                 scaleType: 'time'
             }
-        })
-    }
+        });
+    };
 
     React.useEffect(() => {
-        getDataForS1()
-    }, [])
+        getDataForS1();
+    }, []);
 
-    const [showCalorieIntakePopup, setShowCalorieIntakePopup] = React.useState<boolean>(false)
+    const [showCalorieIntakePopup, setShowCalorieIntakePopup] = React.useState<boolean>(false);
 
     return (
         <div className='reportpage'>
-            <div className='s1'>
-                {
-                    dataS1 &&
-                    <LineChart
-                        xAxis={[{
-                            id: 'Day',
-                            data: dataS1.xAxis.data,
-                            scaleType: dataS1.xAxis.scaleType,
-                            label: dataS1.xAxis.label,
-                            valueFormatter: (date: any) => {
-                                return date.getDate();
-                            }
-                        }]}
-                        series={[
-                            {
+            {['s1', 's2', 's3', 's4'].map((className, index) => (
+                <div key={index} className={className}>
+                    {dataS1 && (
+                        <LineChart
+                            xAxis={[{
+                                id: 'Day',
+                                data: dataS1.xAxis.data,
+                                scaleType: dataS1.xAxis.scaleType,
+                                label: dataS1.xAxis.label,
+                                valueFormatter: (date: Date) => {
+                                    return date.toLocaleDateString(); // Convert the date to a string format
+                                },
+                            }]}
+                            series={[{
                                 data: dataS1.data,
                                 label: dataS1.title,
                                 color: dataS1.color,
-                            },
-                        ]}
-                        {...chartsParams}
-                    />
-                }
-            </div>
-            <div className='s2'>
-                {
-                    dataS1 &&
-                    <LineChart
-                        xAxis={[{
-                            id: 'Day',
-                            data: dataS1.xAxis.data,
-                            scaleType: dataS1.xAxis.scaleType,
-                            label: dataS1.xAxis.label,
-                            valueFormatter: (date: any) => {
-                                return date.getDate();
-                            }
-                        }]}
-                        series={[
-                            {
-                                data: dataS1.data,
-                                label: dataS1.title,
-                                color: dataS1.color,
-                            },
-                        ]}
-                        {...chartsParams}
-                    />
-                }
-            </div>
+                            }]}
+                            {...chartsParams}
+                        />
+                    )}
+                </div>
+            ))}
 
-            <div className='s3'>
-                {
-                    dataS1 &&
-                    <LineChart
-                        xAxis={[{
-                            id: 'Day',
-                            data: dataS1.xAxis.data,
-                            scaleType: dataS1.xAxis.scaleType,
-                            label: dataS1.xAxis.label,
-                            valueFormatter: (date: any) => {
-                                return date.getDate();
-                            }
-                        }]}
-                        series={[
-                            {
-                                data: dataS1.data,
-                                label: dataS1.title,
-                                color: dataS1.color,
-                            },
-                        ]}
-                        {...chartsParams}
-                    />
-                }
-            </div>
-
-            <div className='s4'>
-                {
-                    dataS1 &&
-                    <LineChart
-                        xAxis={[{
-                            id: 'Day',
-                            data: dataS1.xAxis.data,
-                            scaleType: dataS1.xAxis.scaleType,
-                            label: dataS1.xAxis.label,
-                            valueFormatter: (date: any) => {
-                                return date.getDate();
-                            }
-                        }]}
-                        series={[
-                            {
-                                data: dataS1.data,
-                                label: dataS1.title,
-                                color: dataS1.color,
-                            },
-                        ]}
-                        {...chartsParams}
-                    />
-                }
-            </div>
-            <button className='editbutton'
-                onClick={() => {
-                    setShowCalorieIntakePopup(true)
-                }}
+            <button
+                className='editbutton'
+                onClick={() => setShowCalorieIntakePopup(true)}
             >
                 <AiFillEdit />
             </button>
 
-            {
-                showCalorieIntakePopup &&
-
+            {showCalorieIntakePopup && (
                 <CaloriIntakePopup setShowCalorieIntakePopup={setShowCalorieIntakePopup} />
-
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
--- src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
+++ src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const CalorieIntakePopup = () => {
-  return (
-    <div>CalorieIntakePopup</div>
-  )
-}
+type CalorieIntakePopupProps = {
+    setShowCalorieIntakePopup: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-export default CalorieIntakePopup
+const CalorieIntakePopup: React.FC<CalorieIntakePopupProps> = ({ setShowCalorieIntakePopup }) => {
+    return (
+        <div className="popup">
+            <div>Calorie Intake Popup</div>
+            <button onClick={() => setShowCalorieIntakePopup(false)}>Close</button>
+        </div>
+    );
+};
+
+export default CalorieIntakePopup;
