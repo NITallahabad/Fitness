bb62502924fce033e1772b83169045be6dea25ae
amitasingh <amitasingh3173@gmail.com>
2024-11-11 00:38:11 +0530

    error solved

--- Fitness_backend/Models/UserSchema.js
+++ Fitness_backend/Models/UserSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -143,9 +143,9 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this;
 
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
+    // if (user.isModified('password')) {
+    //     user.password = await bcrypt.hash(user.password, 8);
+    // }
 
     next();
 });
