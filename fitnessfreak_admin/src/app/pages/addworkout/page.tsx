a6ddf20971be875ff88739b1d64c720823f42e28
Shresth <142995647+spidy2301@users.noreply.github.com>
2024-11-11 21:44:36 +0530

    Updated admin workout page

--- fitnessfreak_admin/src/app/pages/addworkout/page.tsx
+++ fitnessfreak_admin/src/app/pages/addworkout/page.tsx
@@ -46,7 +46,23 @@ const page = () => {
             [e.target.name]: e.target.value
         })
     }
-    const addExerciseToWorkout = () => {}
+    const handleExerciseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setExercise({
+            ...exercise,
+            [e.target.name]: e.target.value
+        })
+    }
+    const addExerciseToWorkout = () => {
+        console.log(exercise)
+        if(exercise.name=='' || exercise.description=='' || exercise.sets==0 || exercise.reps==0 || exercise.imageFile==null)
+        {
+            toast.error('please fill in the fields',{
+                position: toast.POSITION.TOP_CENTER,
+            });
+            return;  
+        }
+        
+    }
     const deleteExerciseFromWorkout = (index: number) => {}
     const uploadImage = async(image: File) => {}
     const checkLogin = async () => {}
@@ -55,7 +71,105 @@ const page = () => {
   return (
     <div className='formpage'>
         <h1 className='title'>Add Workout</h1>
+        <input
+         type="text"
+         placeholder='Workout Name'
+         name="name"
+         value={workout.name}
+         onChange={handleWorkoutChange}
+         />
+         <textarea
+          placeholder='Workout Description'
+          name="description"
+          value={workout.description}
+          onChange={(e)=>{
+            setWorkout({
+                ...workout,
+                description: e.target.value
+            })
+          }}
+          rows={5}
+          cols={50}
+         />
+         <input
+          type="number"
+          placeholder='WORKOUT duration'
+          name='durationInMinutes'
+          value={workout.durationInMinutes}
+          onChange={handleWorkoutChange}
+         />
+         <input
+          type='file'
+          placeholder='Workout Image'
+          name='workoutImage'
+          onChange={(e)=>
+            setWorkout({
+                ...workout,
+                imageFile: e.target.files![0]
+            })
+          }
+         />
 
+         <div
+          style={{
+            display: 'flex',
+            flexDirection : 'row',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+         >
+            <h2 className='title'>Add Exercise to Workout</h2>
+             <input
+             type='text'
+             placeholder='Exercise name'
+             name='name'
+             value={exercise.name}
+             onChange={handleExerciseChange}
+             />
+             <textarea
+              placeholder='Exercise Description'
+              name='description'
+              value={exercise.description}
+              onChange={(e)=>{
+                setExercise({
+                    ...exercise,
+                    description: e.target.value
+                })
+              }}
+              rows={5}
+              cols={50}
+             />
+             <label htmlFor="sets">Sets</label>
+             <input type="number" 
+              placeholder='sets'
+              name='sets'
+              value={exercise.sets}
+              onChange={handleExerciseChange}
+              />
+              <label htmlFor="reps">Reps</label>
+              <input type="number"
+               placeholder='Reps'
+               name='Reps'
+               value={exercise.reps}
+               onChange={handleExerciseChange}
+              />
+              <input type="file" 
+               placeholder='Exercise Image'
+               name='exerciseImage'
+               onChange={(e)=>{
+                setExercise({
+                    ...exercise,
+                    imageFile: e.target.files![0]
+                })
+               }}
+              />
+              <button
+                 onClick={(e)=>{
+                    addExerciseToWorkout(e)
+                 }}>
+                 Add Exercise
+              </button>
+         </div>
     </div>
   )
 }
